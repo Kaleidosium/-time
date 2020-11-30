d6ea46bb3f111dd599008d7e4bc14dad0ef7c863
iamrifki <iamrifki0@gmail.com>
2020-11-30 11:21:04 +0700

    Push Dev

--- src/components/WorldClock.svelte
+++ src/components/WorldClock.svelte
@@ -8,11 +8,18 @@
   export let editButtonsVisible;
   let displayedTimeDiff;
 
-  const timeZone = "America/Argentina/Buenos_Aires";
+  let timeZones = [
+    { id: 1, timeZone: "America/Argentina/Buenos_Aires" },
+    { id: 2, timeZone: "Europe/Oslo" },
+    { id: 3, timeZone: "Australia/Sydney" },
+  ];
+  let totalTimeZones = timeZones.length;
+
+  //const timeZone = "America/Argentina/Buenos_Aires";
   const pattern = "HH:mm:ss";
   // Get the last name of the full TZ name
   // and replace the underscore with a space
-  const formattedTimeZone = timeZone
+  $: formattedTimeZone = timeZone
     .split("/")
     .slice(-1)
     .pop()
@@ -39,32 +46,32 @@
   <li class="datetime">
     <div class="cards">
       <div class="card">
-        <span class="time-elsewhere label-wrapper">
-          <label for="datetime-1" class="datetime-label">
-            <span>
-              <span class="location">{formattedTimeZone}</span>
-              <br />
-              <span class="hours-behind">{displayedTimeDiff}</span>
-            </span>
-            <span class="world-time">{output}</span>
-          </label>
-        </span>
-        {#if editButtonsVisible}
-          <span class="btn-group">
-            <!-- TODO?(alt): Make this one of those :: things infront of the label instead -->
-            <button
-              type="button"
-              class="btn--secondary"
-              aria-pressed="false">
-              <Fa icon={faArrowsAlt} size="1.2x" />
-              <!-- <span class="visually-hidden">Jakarta</span> -->
-            </button>
-            <button type="button" class="btn--danger" aria-pressed="false">
-              <Fa icon={faTrash} size="1.2x" />
-              <!-- <span class="visually-hidden">Jakarta</span> -->
-            </button>
+        {#each timeZones as timeZone, index (timeZones.id)}
+          <span class="time-elsewhere label-wrapper">
+            <label for="datetime-1" class="datetime-label">
+              <span>
+                <span
+                  class="location">{timeZone.timeZone}</span>
+                <br />
+                <span class="hours-behind">{displayedTimeDiff}</span>
+              </span>
+              <span class="world-time">{output}</span>
+            </label>
           </span>
-        {/if}
+          {#if editButtonsVisible}
+            <span class="btn-group">
+              <!-- TODO?(alt): Make this one of those :: things infront of the label instead -->
+              <button type="button" class="btn--secondary" aria-pressed="false">
+                <Fa icon={faArrowsAlt} size="1.2x" />
+                <!-- <span class="visually-hidden">Jakarta</span> -->
+              </button>
+              <button type="button" class="btn--danger" aria-pressed="false">
+                <Fa icon={faTrash} size="1.2x" />
+                <!-- <span class="visually-hidden">Jakarta</span> -->
+              </button>
+            </span>
+          {/if}
+        {/each}
       </div>
     </div>
   </li>
