559794b11f59aca53bbb312344724bfa0ed42e26
iamrifki <iamrifki0@gmail.com>
2020-12-01 18:07:07 +0700

    Add Datalist Autocomplete cause I'm Lazy

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
+import json from "@rollup/plugin-json";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -77,6 +78,9 @@ export default {
     // If we're building for production (npm run build
     // instead of npm run dev), minify
     production && terser(),
+
+    // For JSON Imports
+    json(),
   ],
   watch: {
     clearScreen: false,
--- src/components/AddWorldClock.svelte
+++ src/components/AddWorldClock.svelte
@@ -1,21 +1,28 @@
 <script>
   import { createEventDispatcher } from "svelte";
+  import timeZoneJSONData from "tzdata";
   import Fa from "svelte-fa";
   import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
   export let newTimeZone;
 
   const dispatch = createEventDispatcher();
+  const timeZoneData = Object.keys(timeZoneJSONData.zones);
 </script>
 
 <form on:submit|preventDefault={dispatch('submitTimeZone', newTimeZone)}>
   <input
     type="text"
     id="datetime-0"
-    autocomplete="off"
+    list="timeZoneList"
     class="input input--lg"
     placeholder="Add timezones"
     bind:value={newTimeZone} />
+  <datalist id="timeZoneList">
+    {#each timeZoneData as listedTimeZone}
+      <option value={listedTimeZone} />
+    {/each}
+  </datalist>
   <button
     type="submit"
     disabled=""
